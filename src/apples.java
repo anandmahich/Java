3d391fd0f945483752e31a7f6868fdb100da4e55
anandmahich <anandsinghmahich@gmail.com>
2018-06-21 23:41:10 +0530

    added apples.java class

---
+++ src/apples.java
@@ -0,0 +1,9 @@
+import javax.swing.JFrame;
+public class apples {
+    public static void main(String[] args) {
+        gui go = new gui();
+        go.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        go.setSize(300,200);
+        go.setVisible(true);
+    }
+}
